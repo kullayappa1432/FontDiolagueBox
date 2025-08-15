4b455fca1881f1a72625f16b3b1744368314ee0c
kullayappa1432 <rallapallikullayappakullayappa@gmail.com>
2025-08-15 12:14:42 +0530

    Add files via upload

---
+++ fontdilogbox/fontdilogbox/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace fontdilogbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.diolagueText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(243, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 177);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "microsoft sanscrif";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Italic",
+            "Bold",
+            "Regular",
+            "Bold Italic"});
+            this.comboBox2.Location = new System.Drawing.Point(381, 43);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 177);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.Text = "Regular";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(517, 43);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 177);
+            this.comboBox3.TabIndex = 2;
+            this.comboBox3.Text = "10";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // diolagueText
+            // 
+            this.diolagueText.Location = new System.Drawing.Point(34, 43);
+            this.diolagueText.Multiline = true;
+            this.diolagueText.Name = "diolagueText";
+            this.diolagueText.Size = new System.Drawing.Size(192, 208);
+            this.diolagueText.TabIndex = 3;
+            this.diolagueText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(255, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "font_name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(397, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "font_style";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(538, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "font_size";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "font_test";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 305);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.diolagueText);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox diolagueText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+
+    }
+}
+
---
+++ fontdilogbox/fontdilogbox/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Text;
+
+namespace fontdilogbox
+{
+    public partial class Form1 : Form
+    {
+        String fname = "Microsoft Sans Serif";
+        float fsize = 10;
+        string fstyle ="Regular";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InstalledFontCollection col = new InstalledFontCollection();
+            foreach (FontFamily f in col.Families)
+            {
+                comboBox1.Items.Add(f.Name);
+            }
+            for (int i = 6; i < 100; i++)
+            {
+                comboBox3.Items.Add(i);
+            }
+    
+
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string fname = comboBox1.Text;
+            float fsize = float.Parse(comboBox3.Text);
+            string fstyle = comboBox2.Text;
+
+            if (fstyle == "Regular")
+            {
+                Font f = new Font(fname, fsize, FontStyle.Regular);
+                diolagueText.Font = f;
+            }
+
+            if (fstyle == "Bold")
+            {
+                Font f = new Font(fname, fsize, FontStyle.Bold);
+                diolagueText.Font = f;
+            }
+
+            if (fstyle == "Italic")
+            {
+                Font f = new Font(fname, fsize, FontStyle.Italic);
+                diolagueText.Font = f;
+            }
+            if (fstyle == "Bold Italic")
+            {
+                Font f = new Font(fname, fsize, FontStyle.Strikeout);
+                diolagueText.Font = f;
+            }
+
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string fname = comboBox1.Text;
+            float fsize = float.Parse(comboBox3.Text);
+            string fstyle = comboBox2.Text;
+
+            if (fstyle == "Regular")
+            {
+                Font f = new Font(fname, fsize, FontStyle.Regular);
+                diolagueText.Font = f;
+            }
+
+            if (fstyle == "Bold")
+            {
+                Font f = new Font(fname, fsize, FontStyle.Bold);
+                diolagueText.Font = f;
+            }
+
+            if (fstyle == "Italic")
+            {
+                Font f = new Font(fname, fsize, FontStyle.Italic);
+                diolagueText.Font = f;
+            }
+            if (fstyle == "Bold Italic")
+            {
+                Font f = new Font(fname, fsize, FontStyle.Strikeout);
+                diolagueText.Font = f;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string fname = comboBox1.Text;
+            float fsize = float.Parse(comboBox3.Text);
+            string fstyle = comboBox2.Text;
+
+            if (fstyle == "Regular")
+            {
+                Font f = new Font(fname, fsize, FontStyle.Regular);
+                diolagueText.Font = f;
+            }
+
+            if (fstyle == "Bold")
+            {
+                Font f = new Font(fname, fsize, FontStyle.Bold);
+                diolagueText.Font = f;
+            }
+
+            if (fstyle == "Italic")
+            {
+                Font f = new Font(fname, fsize, FontStyle.Italic);
+                diolagueText.Font = f;
+            }
+            if (fstyle == "Bold Italic")
+            {
+                Font f = new Font(fname, fsize, FontStyle.Strikeout);
+                diolagueText.Font = f;
+            }
+           
+        }
+    }
+}
